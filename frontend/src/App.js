4cea4ca509f48ee2bab36f5f24ee03ca99c17bda
MZBCodes <mzb.codes@gmail.com>
2024-04-03 01:33:58 -0400

    added in React Material UI and played aorund with some button stuff.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#a4d4b7',
+      main: '#449D69',
+      dark: '#2d5a3e',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#e69ecb',
+      main: '#9d4478',
+      dark: '#753c65',
+      contrastText: '#000',
+    },
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true, // No more ripple, on the whole application 💣!
+      },
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme} >
+        <Button className="button" color="primary" variant='contained' >
+          Hello, Material-UI
+        </Button>
+      </ThemeProvider>
+
     </div>
   );
 }
