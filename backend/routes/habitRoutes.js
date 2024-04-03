2550ee5ff1d34a220bdc349702b218d48e6f130d
MZBCodes <mzb.codes@gmail.com>
2024-04-02 23:06:02 -0400

    added Update and Delete operations for Habit Tracking

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const {verifyAdmin, verifyToken} = require('../services/verifyServices')
+const { verifyAdmin, verifyToken } = require('../services/verifyServices')
 
 router.put('/addHabit', verifyToken, async (req, res) => {
     console.log("Trying to Add Habit");
@@ -33,8 +33,9 @@ router.put('/addHabit', verifyToken, async (req, res) => {
         console.log(associatedUser.habits)
         await associatedUser.save();
 
-        res.response(201).json({ message: "Habit added succesfully" })
-    } catch {
+        res.status(201).json({ message: "Habit added succesfully" })
+    } catch (error) {
+        console.error("Error adding habit,", error)
         res.status(500).json({ message: 'Internal server error' });
     }
 })
@@ -60,9 +61,9 @@ router.get('/getHabits', verifyToken, async (req, res) => {
     }
 })
 
-router.post('./updateHabit', verifyToken, async (req, res) => {
+router.put('/updateHabit', verifyToken, async (req, res) => {
     try {
-        const { email, habitName, description, completionStatus, frequency} = req.body;
+        const { email, name, newName, description, completionStatus, frequency } = req.body;
         console.log(email)
         const associatedUser = await User.findOne({ email })
         console.log(associatedUser)
@@ -71,15 +72,15 @@ router.post('./updateHabit', verifyToken, async (req, res) => {
             return res.status(400).json({ message: "Associated user not found" })
         }
         let newHabit = {
-            name: habitName,
+            name: newName,
             description: description,
             completionStatus, completionStatus,
             frequency: frequency
         }
-        console.log(associatedUser.habits)
         let habitFound = false;
-        for (let i = 0; i < associatedUser.habits; i++){
-            if (associatedUser.habits[i].name == habitName) {
+        for (let i = 0; i < associatedUser.habits.length; i++) {
+            console.log(associatedUser.habits[i].name, name)
+            if (associatedUser.habits[i].name == name) {
                 associatedUser.habits[i] = newHabit;
                 habitFound = true;
                 break;
@@ -88,11 +89,54 @@ router.post('./updateHabit', verifyToken, async (req, res) => {
         if (!habitFound) {
             return res.status(400).json({ message: "Habit of requested name not found" })
         }
+        await associatedUser.save();
         res.json({
             message: "Habits changed succesfully",
             habits: associatedUser.habits
         })
-    } catch {
+    } catch (error) {
+        console.error("Error", error)
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+router.delete('/deleteHabit', verifyToken, async (req, res) => {
+    try {
+        const { email, habitName, } = req.body;
+        console.log(email)
+        const associatedUser = await User.findOne({ email })
+        console.log(associatedUser)
+        if (!associatedUser) {
+            console.log("Bad");
+            return res.status(400).json({ message: "Associated user not found" })
+        }
+        let numHabits = associatedUser.habits.length;
+        let habitFound = false;
+        for (let i = 0; i < associatedUser.habits.length; i++) {
+            console.log(associatedUser.habits[i].name, habitName)
+            if (associatedUser.habits[i].name == habitName) {
+                associatedUser.habits.splice(i, 1);
+                habitFound = true;
+                break;
+            }
+        }
+        if (!habitFound) {
+            return res.status(400).json({ message: "Habit of requested name not found" })
+        }
+        await associatedUser.save();
+
+        let numNewHabits = associatedUser.habits.length;
+        if (numNewHabits >= numHabits) {
+            return res.status(400).json({ message: "Habit not deleted correctly" })
+        }
+        
+        res.json({
+            message: "Habit deleted succesfully",
+            habits: associatedUser.habits
+        })
+
+    } catch (err) {
+        console.error(err);
         res.status(500).json({ message: 'Internal server error' });
     }
 })
